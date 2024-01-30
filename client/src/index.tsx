05a3909a6df1e54c43bbe123b1d0f41701a25ff8
ritaaktay <rita.aktay@vodaofone.com>
2024-01-30 16:16:54 +0000

    Clean out boilerplate React

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./utils/reportWebVitals";
 import Home from "./Home";
 
 const root = ReactDOM.createRoot(
